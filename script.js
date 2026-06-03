7a0c7cdf2b8130f9c43f58a06b460a6dac85c814
stella-quynhthy <stellaqtnguyen@gmail.com>
2026-06-03 18:00:46 -0400

    started 2day

---
+++ script.js
@@ -0,0 +1,4 @@
+const emojis = ["🍎","🦊","🐣","🐹","🐰","🐺","🐷","🐶","🐻‍❄️","🍓"];
+let scoreDisplay = document.getElementById("score");
+let targetDisplay = document.getElementById("target");
+let gameInterval; 
