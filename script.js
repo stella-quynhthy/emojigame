4b332b9c77e6d888ca849243b00592525d32ddc9
stella-quynhthy <stellaqtnguyen@gmail.com>
2026-06-10 18:03:26 -0400

    need to finish

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-const emojis = ["🍎","🦊","🐣","🐹","🐰","🐺","🐷","🐶","🐻‍❄️","🍓"];
+//----------------------------------HTML Elements-------------------------------------------
+const emojis = ["🍎","🦊","🐣","🐿️","🐰","🐺","🐷","🐶","🐻‍❄️","🥟"];
 let scoreDisplay = document.getElementById("score");
 let targetDisplay = document.getElementById("target");
-let gameInterval; 
+let startBtn = document.getElementById("start");
+const gameContainer = document.getElementById("gameContainer");
+let score = 0;
+scoreDisplay.textContent = score;
+let gameInterval;
+
+function targetEmoji() {
+    let randomIndex = Math.floor(Math.random()*emojis.length);
+    let randomEmoji = emojis[randomIndex];
+    targetDisplay.textContent = randomEmoji;
+};
+
+function showEmojis() {
+    for (let i = 0; i < emojis.length; i++) {
+        let randomIndex = Math.floor(Math.random()*emojis.length);
+        let emoji = emojis[randomIndex];
+        let button = document.createElement("button");
+        button.textContent = emoji;
+        gameContainer.appendChild(button);
+    }
+}
+
+function startGame() {
+    targetEmoji();
+}
+
+startBtn.addEventListener("click", startGame)
+
+showEmojis();
