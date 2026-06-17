3fc261c757e9f82282c264c5f0252a19d947f1f3
s <stellaqtnguyen@gmail.com>
2026-06-17 18:03:34 -0400

    almost done i think :)

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //----------------------------------HTML Elements-------------------------------------------
-const emojis = ["🍎","🦊","🐣","🐿️","🐰","🐺","🐷","🐶","🐻‍❄️","🥟"];
+const emojis = ["🦊","🐶","🐣","🐿️","🥟","🐰","🐷","🐺","🍎","🐻‍❄️"];
 let scoreDisplay = document.getElementById("score");
 let targetDisplay = document.getElementById("target");
 let startBtn = document.getElementById("start");
@@ -8,26 +8,44 @@ let score = 0;
 scoreDisplay.textContent = score;
 let gameInterval;
 
+//-------------------------------Target----------------------------------------------------
+
 function targetEmoji() {
+    clearInterval(gameInterval);
     let randomIndex = Math.floor(Math.random()*emojis.length);
     let randomEmoji = emojis[randomIndex];
     targetDisplay.textContent = randomEmoji;
+    gameInterval = setInterval(showEmojis, 300);
 };
-
+ //------------------------------Show Emojis on Screen---------------------------------
 function showEmojis() {
-    for (let i = 0; i < emojis.length; i++) {
-        let randomIndex = Math.floor(Math.random()*emojis.length);
-        let emoji = emojis[randomIndex];
-        let button = document.createElement("button");
+    //for (let i = 0; i < emojis.length; i++) {
+        const emoji = emojis[Math.floor(Math.random()*emojis.length)];
+        //let emoji = emojis[randomIndex];
+        const button = document.createElement("button");
         button.textContent = emoji;
+        button.className = emoji;
+        button.style.position = "absolute";
+        button.style.left = Math.random() * 90 + "%";
+        button.style.top = Math.random() * 90 + "%";
+        button.style.transform = "translate(-50%, -50%)";
+        button.style.fontSize = "30px";
+
+        button.addEventListener("click", () => {
+            if (emoji === targetDisplay.textContent) {
+                score++;
+                scoreDisplay.textContent = score;
+            }
+            button.remove();
+        });
         gameContainer.appendChild(button);
-    }
-}
+        setTimeout(()=>button.remove(), 1000)
+        }
+//}
 
 function startGame() {
     targetEmoji();
 }
 
 startBtn.addEventListener("click", startGame)
-
 showEmojis();
